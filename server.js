99d7e2b4bd89b2ad84d9b3b32cdf052f3f5c5992
BetterhavemyMonet <xomonetox168@outlook.com>
2026-06-07 13:19:44 -0500

    Set entry fee to 10 MONET

--- server.js
+++ server.js
@@ -864,7 +864,7 @@ app.get('/api/monet-price', async (_req, res) => {
   res.json({
     ok: true,
     priceUsd: 0.0099,
-    entryFeeMonet: 100,
+    entryFeeMonet: 10,
     entryFeeUsd: 0.99,
     solPriceUsd,
     solEntryLamports,
